82bc33a61ddbc7bab4db239d8890934b926a809e
ScrollDude <stepakinoyan@gmail.com>
2025-04-28 22:45:00 +0900

    build(poetry): update fastapi depends

--- app/tests/integration_tests/test_filter_stats.py
+++ app/tests/integration_tests/test_filter_stats.py
@@ -3,22 +3,25 @@ from httpx import AsyncClient
 
 
 @pytest.mark.parametrize(
-    "level,founder,sphere",
+    "level,founder,sphere,date_from,date_to",
     [
-        ("МО", "Благовещенск", "Школы"),
-        ("МО", "Селемджинский", "Культура"),
+        ("МО", "Благовещенск", "Школы", "2023-01-01", "2023-12-31"),
+        ("МО", "Селемджинский", "Культура", "2023-01-01", "2023-12-31"),
     ],
 )
-async def test_get_founder_by_level(
-    level: str, founder: str, sphere: str, authenticated_ac: AsyncClient
+async def test_get_stats(
+    level: str, founder: str, sphere: str, date_from: str, date_to: str, authenticated_ac: AsyncClient
 ):
     response = await authenticated_ac.get(
         "/filter/get_stats",
-        params={"level": level, "founder": founder, "sphere": sphere},
+        params={
+            "level": level,
+            "founder": founder,
+            "sphere": sphere,
+            "date_from": date_from,
+            "date_to": date_to,
+        },
     )
-
     data = response.json()
-
-
     assert isinstance(data, list)
     assert response.status_code == 200
